fcbf44051f2d11915c4bbbf63f44a5748f793e87
mhenn <mikeianhenning93@gmail.com>
2018-04-29 19:41:52 +0200

    finished ue2

--- Uebungen/Uebung 2/Aufgabe 3.pl
+++ Uebungen/Uebung 2/Aufgabe 3.pl
@@ -10,5 +10,13 @@ add(s(X),Y,s(R)) :- add(X,Y,R),natSym(X), natSym(Y),natSym(R).
 mult(X, o, o).
 mult(X, s(Y), R):- mult(X,Y, Zwischen), add(Zwischen, X, R).
 
-exp(X, o, s(o)).
-exp(X,s(Y),R) :-   exp(X,Y,K) ,mult(K, X,R).
+%Aufgabenteil a
+%X hoch Y ergibt R
+exp(X, o, s(o)):- natSym(X).
+exp(X,s(Y),R) :-   exp(X,Y,K) ,mult(K, X,R), natSym(X), natSym(Y), natSym(R).
+
+%Aufgabenteil b
+%Logarithmus  exp( Basis , Ergebnis , Wert)
+
+%Aufgabenteil c
+% Nein, wenn durch natsym
--- Uebungen/Uebung 2/Aufgabe12.pl
+++ Uebungen/Uebung 2/Aufgabe12.pl
@@ -3,9 +3,24 @@
 %Gibt das erste Element (X) fuer die liste Xs aus
 
 
-
+%Erstes Element X ist head der Liste
 head(list(X,nil),X).
-head(list(X,Rest),X).
+head(list(X,Xs),X).
 
+%Letztes Element Y ist tail der Liste(X,Xs)
 tail(list(X,nil),X).
-tail(list(Xs,Rest),X):- tail(Rest, X).
+tail(list(X,Xs),Y):- tail(Xs, Y).
+
+%Die Liste mit head R
+append(nil,list(Y,nil), list(Y,nil)).
+append(nil,list(Y,Ys),list(Y,Rs)):- append(nil, Ys ,Rs).
+append(list(X,nil),list(Y,Ys),list(X,Rs)):- append(nil,list(Y,Ys),Rs).
+append(list(X,Xs),list(Y,Ys),list(X,Rs)):- append(Xs,list(Y,Ys),Rs).
+
+
+
+
+anothTest(list(X,nil),list(X,nil)).
+anothTest(list(X,Xs),list(X,Rs)):- anothTest(Xs,Rs).
+
+
