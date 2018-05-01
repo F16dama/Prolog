d60383f93eb44696d3a5bdfdec04b5ce0587202f
mhenn <mikeianhenning93@gmail.com>
2018-05-01 21:59:12 +0200

    finished prak

--- Praktika/Praktikum 1/familyRelations.pl
+++ Praktika/Praktikum 1/familyRelations.pl
@@ -36,10 +36,10 @@ sohn(X,Y):- maennlich(X), elternteil(Y,X).
 tochter(X,Y):- weiblich(X), elternteil(Y,X).
 
 %X ist Bruder von Y
-bruder(X,Y):- maennlich(X), elternteil(Par,X),elternteil(Par,Y), X\==Y.
+bruder(X,Y):- maennlich(X), elternteil(Par,X),elternteil(Par,Y), X\=Y.
 
 %X ist Schwester von Y
-schwester(X,Y):- maennlich(X), elternteil(Par,X),elternteil(Par,Y).
+schwester(X,Y):- maennlich(X), elternteil(Par,X),elternteil(Par,Y), X\=Y.
 
 %X ist grossvater von Y
 grossVater(X,Y):- maennlich(X), elternteil(X,C), elternteil(C,Y).
@@ -50,8 +50,14 @@ grossMutter(X,Y):- weiblich(X), elternteil(X,C), elternteil(C,Y).
 %X ist Onkel von Y
 onkel(X,Y):- bruder(X,S), elternteil(S,Y).
 %X ist tante von Y
-tante(X,Y).
+tante(X,Y):- schwester(X,S), elternteil(S,Y).
+
 %X ist cousin von Y
-cousin(X,Y).
+cousin(X,Y):- onkel(O,Y),elternteil(O,X).
+
+%X ist vorfahre von Y
+vorfahre(X,Y):- elternteil(X,Y).
+vorfahre(X,Y):- elternteil(X,C),vorfahre(C,Y).
+
 
 
