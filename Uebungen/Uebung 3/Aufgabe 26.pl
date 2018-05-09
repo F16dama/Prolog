72fad33d61ad550270927e536ee9bee573ccedb8
mhenn <mikeianhenning93@gmail.com>
2018-05-09 09:10:39 +0200

    stuff

--- Uebungen/Uebung 3/Aufgabe 26.pl
+++ Uebungen/Uebung 3/Aufgabe 26.pl
@@ -1,4 +1,16 @@
 % Author:
 % Date: 5/4/2018
 
-tree(a,f(b,g(c)),h(vX,vY,vZ)).
+const(a).
+const(b).
+const(c).
+
+vari(vX).
+vari(vY).
+vari(vZ).
+
+tree(X):- const(X).
+tree(X):- vari(X).
+tree(Xb):- tree(Xb).
+tree(f(X1b, X2b)):-tree(X1b), tree(X2b).
+tree(h(X1b,X2b,X3b)):- tree(X1b), tree(X2b), tree(X3b).
