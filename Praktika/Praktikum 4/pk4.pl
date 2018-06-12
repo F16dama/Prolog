453041eb74d82d0d6036e7b0b9c32f882d29758f
mhenn <mikeianhenning93@gmail.com>
2018-06-12 12:46:06 +0200

    finished the fuck, such a good boi wolf

--- Praktika/Praktikum 4/pk4.pl
+++ Praktika/Praktikum 4/pk4.pl
@@ -8,14 +8,17 @@
 %z0 = {z0}
 % E = {z3}
 
+%endliche Zustandsmenge
 eZ(z0).
 eZ(z1).
 eZ(z2).
 eZ(z3).
 
+%Alphabet sigma
 sigma(tick).
 sigma(tack).
 
+%Transitionsfunktion delta (zustandAlt,uebergangsZeichen, zustandNeu)
 delta(z0,tick,z1).
 delta(z1,tack,z3).
 delta(z1,tack,z2).
@@ -27,15 +30,18 @@ delta(z1,[],z1).
 delta(z2,[],z2).
 delta(z3,[],z3).
 
+%Startzustand z0
 startZ(z0).
+%Endzustand E
 endZ(z3).
 
 %b
 
+%Transformatorfunktion (Zalt, zeichenListe, Zneu)
 delta_stern(Z,[],Z):- eZ(Z), delta(Z,[],Z).
 delta_stern(Z,[A|Ws],ZStrich):- delta(Z,A,ZStern), delta_stern(ZStern, Ws,ZStrich).
 
 sigma_stern([]).
 sigma_stern([A|Ws]):- sigma(A), sigma_stern(Ws).
 
-lvonN(W):-  delta_stern().
+lvonN(Ws):-  startZ(Z0),sigma_stern(Ws),endZ(E),delta_stern(Z0,Ws,E).
